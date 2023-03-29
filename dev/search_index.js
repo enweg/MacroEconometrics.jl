fd48b1df9ca35bda82ff18794f0dbdaf83eb844a
Documenter.jl <documenter@juliadocs.github.io>
2023-03-29 19:00:02 +0000

    build based on ffba59d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MacroEconometrics","category":"page"},{"location":"#MacroEconometrics","page":"Home","title":"MacroEconometrics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MacroEconometrics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MacroEconometrics]","category":"page"}]
+}
