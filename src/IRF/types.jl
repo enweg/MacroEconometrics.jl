85c31ecdf1ee8f3fa6404f61b3d84e1e16c20ee4
Enrico Wegner <wegner1996@gmail.com>
2023-04-11 13:57:09 +0200

    adding renormalisation

--- src/IRF/types.jl
+++ src/IRF/types.jl
@@ -31,3 +31,46 @@ struct StructuralImpulseResponseFunction{E<:Estimated,M<:MacroEconometricModel,N
     normalisation::N
 end
 
+"""
+
+Move from covariance normalisation to impact normalisation. 
+
+Given a SVAR of the form 
+
+```math
+A y_t = b_0 + B_1 y_{t-1} + ... + B_p y_{t-p} + \Sigma^{1/2}\varepsilon
+```
+
+that has been covariance normalised, such that ``\Sigma^{1/2}=I``, we can move
+to a SVAR that is impact normalised - the diagonal of ``A`` consists of only
+ones, by premultiplying all matriced by ``N = Diag(A)^{-1}`` where ``Diag(X)``
+denotes the matrix of the diagonal of ``X``. The covariance matrix of the
+structural errors is then given by ``NN'``. 
+
+## Arguments 
+
+- `sirfs::<:StructuralImpulseResponseFunction` obtained from a [`VAR`](@ref) or
+  [`SVAR`](@ref) model and using any identification method relying on covariance
+  normalisation. 
+"""
+function to_impact_normalisation(sirfs::S) where {S <: StructuralImpulseResponseFunction} end
+
+# TODO: test me
+function to_impact_normalisation(sirfs::StructuralImpulseResponseFunction{E, M, N}) where {E<:BayesianEstimated, M<:AbstractVectorAutoregression, N<:AbstractIRFNormalisation}
+  irfs = copy(sirfs.irfs.value)
+  horizons = size(irfs, 3)-1
+  for chain in axes(irfs, 5)
+    for draw in axes(irfs, 4)
+      N = diagm(1 ./ diag(irfs[:, :, 1, draw, chain]))
+      for h in 0:horizons
+        irfs[:,:,h+1,draw,chain] = irfs[:,:,h+1,draw,chain] * N
+      end
+    end
+  end
+  return StructuralImpulseResponseFunction(
+    sirfs.variables, 
+    BayesianEstimated(irfs, nothing), 
+    sirfs.model, 
+    IRFImpactNormalisation()
+  )
+end
